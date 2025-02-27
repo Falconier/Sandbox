520f6b5ee17b5c3f93a1eea6f2a7863f4422b9ba
Jacob Emory Bullin <jacobbullin@roadrunner.com>
2025-02-26 22:52:01 -0500

    Ghidra setup and reverse sandbox added

--- InstallProjects/Fresh_Installs.sh
+++ InstallProjects/Fresh_Installs.sh
@@ -2,3 +2,6 @@ apt update
 apt upgrade
 apt install -y net-tools curl unzip wget git
 curl -fsSL https://tailscale.com/install.sh | sh
+
+# If LDE (Lightweight Desktop Environment) is needed
+# apt install xfce4 xfce4-goodies
