71e52254544b5fa9d739a2a87fa0457a018b33ce
Jacob Emory Bullin <jacobbullin@roadrunner.com>
2025-03-18 19:11:18 -0400

    Updated themes

--- InstallProjects/Fresh_Installs.sh
+++ InstallProjects/Fresh_Installs.sh
@@ -1,7 +1,8 @@
 apt update
 apt upgrade
-apt install -y net-tools curl unzip wget git
+apt install -y net-tools curl unzip wget git tree
 curl -fsSL https://tailscale.com/install.sh | sh
 
 # If LDE (Lightweight Desktop Environment) is needed
 # apt install xfce4 xfce4-goodies
+
--- InstallProjects/MSQLServer.sh
+++ InstallProjects/MSQLServer.sh
@@ -1,19 +1,13 @@
 # Ubuntu 22.04
 apt update
 apt upgrade -y
-apt install software-properties-common gpg -y
+apt install curl software-properties-common gpg -y
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+# Next line may be required if public key is not in the keyring
+# curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
 curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | tee /etc/apt/sources.list.d/mssql-server-2022.list
-
-
-
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list)"
-add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/22.04/prod.list)"
 apt update
 apt upgrade -y
-# Verify repository is added
-apt-cache policy mssql-server
 apt install -y mssql-server
 # Run the configuration script
 /opt/mssql/bin/mssql-conf setup
